af3a861e8b8b3b9e56826eb8df9440af10fe4c45
Brian Bixby <brianbixby0@gmail.com>
2018-07-10 17:38:31 -0700

    code is very messy and kinda hacky but it works

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: bbixby <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/09 13:32:44 by bbixby            #+#    #+#             */
-/*   Updated: 2018/07/10 00:06:22 by bbixby           ###   ########.fr       */
+/*   Updated: 2018/07/10 14:54:06 by bbixby           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,88 +31,98 @@ int		ft_min(int a, int b, int c)
 	return (m);
 }
 
-void print(char *arr, int m, int n)
+void print(char *arr)
 {
-	int i;
-	int j;
-
-	i = -1;
-	while (++i < m)
-	{
-		j = -1;
-		while (++j < n)
-		{
-			printf("%c", *((arr+i*n) + j));
-		}
-		printf("%c", '\n');
-	}
+	while (*arr)
+		ft_putchar(*arr++);
 }
 
 void iprint(int *arr, int m, int n)
 {
 	int i;
 	int j;
+	int k;
 
 	i = -1;
+	k = 0;
 	while (++i < m)
 	{
 		j = -1;
 		while (++j < n)
 		{
-			printf("%d", *((arr+i*n) + j));
+			printf("%d", *arr);
+			k++;
 		}
+		printf("%c", '\n');
+		printf("%d", *arr);
 		printf("%c", '\n');
 	}
 }
 
-void	ft_printbsq(bsq_struct *bsq)
+void	ft_fillbsq(bsq_struct *bsq)
 {
-	int		i;
-	int		j;
-	int		x;
-	int		y;
+	int i;
+	int j;
+	int beg_x;
+	int beg_y;
 
-	i = 0;
-	x = bsq->max_x;
-	y = bsq->max_y;
-	while (i < bsq->max_square_size)
+	i = -1;
+	beg_x = bsq->max_x + 1 - bsq->max_square_size;
+	beg_y = bsq->max_y + 1 - bsq->max_square_size;
+	printf("%d", bsq->max_square_size);
+	printf("%c", '\n');
+	while (++i <= bsq->max_square_size)
 	{
 		j = -1;
 		while (++j < bsq->max_square_size)
 		{
-			*((bsq->bsq_copy + (bsq->max_square_size * bsq->cols - (i * bsq->cols)) + bsq->cols - y - j)) = bsq->full;
+			if (beg_y + i == 1)
+			{
+			bsq->bsq_copy[beg_x + j] = bsq->full;
+			}
+			else
+			{
+			bsq->bsq_copy[(beg_y + i - 1) * (bsq->cols) + beg_x + j] = bsq->full;
+			}
 		}
-		i++;
 	}
-	print((char *)bsq->bsq_copy, bsq->rows, bsq->cols);
+	ft_putchar('\n');
+	print(bsq->bsq_copy);
 }
 
 void	ft_create_copies(bsq_struct *bsq, char **argv)
 {
-	char	cdup[bsq->rows][bsq->cols - 1];
+	char 	*cdup;
 	int		idup[bsq->rows][bsq->cols - 1];
 	int i;
 	int j;
 	int b;
-	int max_square_size;
+	int k;
 
 	int fd;
 	int ret;
 	fd = open(*argv, O_RDONLY);
 	char *buf;
-	max_square_size = 0;
-
+	bsq->max_square_size = 0;
+	cdup = (char *)malloc(sizeof(char) * (bsq->rows * bsq->cols + 1));
 	buf = (char *)malloc(sizeof(char) * BUF_SIZE);
 	i = 0;
 	j = 0;
 	b = 0;
+	k = 0;
 	ft_putchar('\n');
 	while ((ret = read(fd, buf, BUF_SIZE )))
 	{
 		while (b < bsq->r1_offset)
+		{
+			printf("%d", b);
+			printf("%c", '\n');
 			b++;
+		}
 		while (b - bsq->r1_offset < bsq->rows * bsq->cols)
 		{
+			cdup[k] = buf[b];
+			k++;
 			if (buf[b] == '\n')
 			{
     			i++;
@@ -123,7 +133,6 @@ void	ft_create_copies(bsq_struct *bsq, char **argv)
   			}
 			else
   			{
-    			cdup[i][j] = buf[b];
 				if (i == 0 || j == 0)
 					if (buf[b] == bsq->empty)
 						idup[i][j] = 1;
@@ -135,25 +144,26 @@ void	ft_create_copies(bsq_struct *bsq, char **argv)
     			}
     			else
       				idup[i][j] = 0;
-				if (idup[i][j] > max_square_size)
+				if (idup[i][j] > bsq->max_square_size)
 				{
 					bsq->max_square_size = idup[i][j];
-					bsq->max_y = i - 1;
-					bsq->max_x = j - bsq->r1_offset + 2;
+					bsq->max_y = (i);
+					bsq->max_x = j;
 				}
-				printf("%d" ,idup[i][j]);
+				printf("%d",idup[i][j]);
     			j++;
 				b++;
   			}
 		}
-		printf("%d", bsq->max_x);
-		printf("%d", bsq->max_y);
+		printf("max square size %d \n", bsq->max_square_size);
+		printf("max x %d \n", bsq->max_x);
+		printf("max y %d \n", bsq->max_y);
 		buf[ret] = '\0';
-		bsq->bsq_copy = &cdup[0][0];
+		bsq->bsq_copy = cdup;
 	}
-	iprint((int *)&idup[0][0], bsq->rows, bsq->cols);
-	print((char *)bsq->bsq_copy, bsq->rows, bsq->cols);
-	ft_printbsq(bsq);
+	/* iprint((int *)&idup[0][0], bsq->rows, bsq->cols); */
+	print(bsq->bsq_copy);
+	ft_fillbsq(bsq);
 }
 
 void	ft_readfile(char **argv)
@@ -165,13 +175,10 @@ void	ft_readfile(char **argv)
 	int 	have_rows;
 	char 	rows[100];
 	char *buf;
-
-	buf = (char *)malloc(sizeof(char) * BUF_SIZE);
-
 	bsq_struct	*bsq;
 
+	buf = (char *)malloc(sizeof(char) * BUF_SIZE);
   	bsq = malloc(sizeof(bsq_struct));
-
 	fd = open(*argv, O_RDONLY);
 	i = 0;
 	have_rows = 0;
@@ -210,6 +217,8 @@ void	ft_readfile(char **argv)
   ft_putnum(bsq->rows);
   ft_putchar('\n');
   ft_putnum(bsq->cols);
+  ft_putchar('\n');
+  ft_putnum(bsq->r1_offset);
   ft_create_copies(bsq, argv);
 }
 
